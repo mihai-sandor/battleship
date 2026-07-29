fad47ef81038731d4b91fb4b5e6e983bc9ad4564
Davide <122094907+Davide13222@users.noreply.github.com>
2026-07-29 15:24:50 +0300

    task 2.1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,18 @@
+function createEmptyMap() {   // 1. Arrays (source of truth)
+    const map = [];
+    for (let row = 0; row < 10; row++) {
+        map.push(new Array(10).fill('apa'));
+    }
+    return map;
+}
+
+const myMap = createEmptyMap();
+const enemyMap = createEmptyMap();
+
+function letterToIndex(letter) {   // 2. Letter -> index conversion function
+    return letter.toUpperCase().charCodeAt(0) - 65;
+}
+
 function createBoard(boardElement) {
     for (let row = 0; row <= 10; row++) {
         for (let col = 0; col <= 10; col++) {
@@ -28,3 +43,8 @@ createBoard(document.getElementById('enemyBoard'));
 // Console log just to count the numbers of elements created on each board
 console.log(document.querySelectorAll('#myBoard .cell').length);
 console.log(document.querySelectorAll('#enemyBoard .cell').length);
+
+const row = letterToIndex("C");  // 2
+const col = 6 - 1;               // 5
+
+myMap[row][col] = "nava";
