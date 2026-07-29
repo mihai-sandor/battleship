506b81016533d005bf2ae66203b4898218007fd8
Sandor Mihai <mihai93dh@gmail.com>
2026-07-29 05:54:45 +0300

    feat: add CSS grid styling for boards, buttons and cell states

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,7 @@
 function createBoard(boardElement) {
     for (let row = 0; row < 10; row++) {
         const rowLetter = String.fromCharCode(65 + row);
-        debugger;
-
+       
         for (let col = 1; col <= 10; col++) {
             const cell = document.createElement('div');
             cell.classList.add('cell');
@@ -10,11 +9,14 @@ function createBoard(boardElement) {
             cell.dataset.col = col;
 
             boardElement.appendChild(cell);
-            debugger;
+         
         }
     }
 }
+
 createBoard(document.getElementById('myBoard'));
 createBoard(document.getElementById('enemyBoard'));
+
+// Console log just to count the numbers of elements created on each board
 console.log(document.querySelectorAll('#myBoard .cell').length);
 console.log(document.querySelectorAll('#enemyBoard .cell').length);
